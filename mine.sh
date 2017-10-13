f3d2ff44bf04fbe127fd9750c32519ee00e0f9c2
Roy Wolfe <mail@royderwolf.de>
2017-10-13 23:41:10 +0100

    Use env var for address. wallet log doesn't need zcashd anymore.

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-~/zen_miner/miner --server eu1-zcash.flypool.org --user t1RYAL9PyuLHaE8rhAfzeiXUsGYo3wXMQm7.miner2 --pass x --port 3333 --templimit 73 --fee 0.01 --api
+~/zen_miner/miner --server eu1-zcash.flypool.org --user "${ZECADDRESS}.miner1" --pass x --port 3333 --templimit 73 --fee 0.01 --api
