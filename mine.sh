70e24f21001ca1f55a276283b0aec02c65091a85
Roy Wolfe <mail@royderwolf.de>
2017-10-19 00:38:03 +0100

    Display address and name on miner start

--- mine.sh
+++ mine.sh
@@ -1,1 +1,2 @@
+echo "Mining for: ${ZECADDRESS}. Name: ${MINERNAME}."
 ~/zen_miner/miner --server eu1-zcash.flypool.org --user "${ZECADDRESS}.${MINERNAME}" --pass x --port 3333 --templimit 73 --fee 0.01 --api
